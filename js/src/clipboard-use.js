1b12448a536133df62c3cb89bb7603202514ce33
ctytl <869721150@qq.com>
2021-11-18 02:58:26 +0800

    Site updated: 2021-11-18 02:58:26

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,33 +1,22 @@
-$(".highlight").wrap("<div class='code-wrapper' style='position:relative'></div>");
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) {
-    /* code */
-    var initCopyCode = function () {
-        var copyHtml = '';
-        copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-        copyHtml += '  <i class="fa fa-clipboard"></i><span>复制</span>';
-        copyHtml += '</button>';
-        $(".highlight .code").before(copyHtml);
-        var clipboard = new ClipboardJS('.btn-copy', {
-            target: function (trigger) {
-                return trigger.nextElementSibling;
-            }
-        });
-        clipboard.on('success', function (e) {
-            e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制成功</span>"
-            setTimeout(function () {
-                e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制</span>"
-            }, 1000)
-           
-            e.clearSelection();
-        });
-        clipboard.on('error', function (e) {
-            e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制失败</span>"
-            setTimeout(function () {
-                e.trigger.innerHTML = "<i class='fa fa-clipboard'></i><span>复制</span>"
-            }, 1000)
-            e.clearSelection();
-        });
-    }
-    initCopyCode();
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
+    copyHtml += '</button>';
+    $(".highlight").wrap($('<div class="highlight-wrap"></div>'));
+    $('.highlight-wrap').prepend(copyHtml);
+    var clipboard = new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+    // 成功提示
+    clipboard.on('success', function(e) {
+      toastPlug('复制成功!', 2000)
+    });
+  }
+  initCopyCode();
 }(window, document);
